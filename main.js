0af85cc8662c989f2ee404dd0a0626a5dba6fcd3
genaroibc <bonavitaismael@gmail.com>
2022-06-12 13:18:44 -0300

    adding submit event in main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const $ = (selector)=> document.querySelector(selector)
+const $ENTRY_FORM = $(".entry__form")
+const $TRY_TEMPLATE = $("#try-template").content
+const $TRIES_SECTION = $(".tries")
+const $FRAGMENT = document.createDocumentFragment()
+
+$ENTRY_FORM.addEventListener("submit", (e)=> {
+  e.preventDefault()
+  const ENTRY = $ENTRY_FORM.combination.value
+  
+  if(ENTRY.length !== 6) return console.log("insert max 6 digits");
+  const $CLONE = $TRY_TEMPLATE.cloneNode(true)
+  const $TEMPLATE_BOXES = $CLONE.querySelectorAll(".digit")
+  for (let i = 0; i < ENTRY.toString().length; i++) {
+    $TEMPLATE_BOXES[i].textContent = ENTRY[i]
+  }
+
+  $TRIES_SECTION.appendChild($CLONE)
+})

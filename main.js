1fde1e5ea544f9daa5fa1bf3e439131467d4568f
genaroibc <bonavitaismael@gmail.com>
2022-06-14 13:18:23 -0300

    optimizing main.js

--- main.js
+++ main.js
@@ -6,13 +6,18 @@ const $TRY_TEMPLATE = $("#try-template").content;
 const $TRIES_SECTION = $(".tries");
 const $FRAGMENT = document.createDocumentFragment();
 const $TEST_PGPH = $("#test-pgph");
-const $PASSWORD_DIGITS = $$(".password__digit");
+// const $PASSWORD_DIGITS = $$(".password__digit");
+const PASSWORD_DIGITS = []
 
-$PASSWORD_DIGITS.forEach((digit) => {
-  digit.textContent = randomNum(0, 9);
-  $TEST_PGPH.insertAdjacentText("beforeend", digit.textContent);
+for(let i = 0; i < 6; i++) {
+  PASSWORD_DIGITS.push(String(randomNum(0,9)))
+}
+
+PASSWORD_DIGITS.forEach((digit) => {
+  $TEST_PGPH.insertAdjacentText("beforeend", digit);
+  // digit.textContent = randomNum(0, 9);
 });
-
+console.log(PASSWORD_DIGITS);
 $ENTRY_FORM.addEventListener("submit", (e) => {
   e.preventDefault();
   const ENTRY = $ENTRY_FORM.combination.value;
@@ -23,17 +28,22 @@ $ENTRY_FORM.addEventListener("submit", (e) => {
 });
 
 function insertTryLine(number) {
+  console.log({number});
   const $CLONE = $TRY_TEMPLATE.cloneNode(true);
   const $TEMPLATE_BOXES = $CLONE.querySelectorAll(".digit");
-  const PASSWORD_DIGITS = Array.from($PASSWORD_DIGITS).map(
-    (el) => el.textContent
-  );
+  // const PASSWORD_DIGITS = Array.from($PASSWORD_DIGITS).map(
+  //   (el) => el.textContent
+  // );
 
   const COMBINATIONS = [];
   let splicedPasswordDigits = structuredClone(PASSWORD_DIGITS);
   let greenPoints = 0;
   console.log({ COMBINATIONS });
   for (let i = 0; i < number.toString().length; i++) {
+    console.log("HOLA");
+    console.log(PASSWORD_DIGITS[i]);
+    console.log(number[i]);
+    console.log(PASSWORD_DIGITS[i] === number[i]);
     $TEMPLATE_BOXES[i].textContent = number[i];
     if (PASSWORD_DIGITS[i] === number[i]) {
       console.log("PASSWORD_DIGITS[i] === number[i]");
@@ -48,7 +58,7 @@ function insertTryLine(number) {
       );
       console.log(splicedPasswordDigits);
       console.log("spliced" + splicedPasswordDigits.splice(
-        splicedPasswordDigits.find((num) => num === number[i]),
+        splicedPasswordDigits.findIndex((num) => num === number[i]),
         1
       ));
       console.log(splicedPasswordDigits);
@@ -66,12 +76,6 @@ function insertTryLine(number) {
     // console.log(COMBINATIONS[i]);
     // console.log($POINTS[i]);
   }
-
+  
   $TRIES_SECTION.appendChild($CLONE);
 }
-
-// function checkPointsTable() {
-//   $PASSWORD_DIGITS.forEach(digit => {
-//     if(digit)
-//   })
-// }
